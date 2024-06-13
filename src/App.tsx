83f38d35704d659dbb838781f371d39eadcbf1c2
02-ahmed <ahmedboabae2@gmail.com>
2024-06-13 20:57:12 +0000

    worked on an expense tracker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import { useState } from "react";
 import Like from "./components/Like";
-import ExpenseTracker from "./components/ExpenseTracker";
+import ExpenseTracker from "./components/expense-tracker/components/ExpenseTracker";
+import ExpenseList from "./components/expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
+import categories from "./components/expense-tracker/categories";
 
 function App() {
   const handleClick = (event:React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   }
+
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const [expenses, setExpenses] = useState([
+    {id:1, description: 'spotify', amount: 12, category:'Entertainment'},
+    {id:2, description: 'milk', amount: 20, category:'Groceries'},
+    {id:3, description: 'water', amount: 5, category:'Utilities'},
+    {id:4, description: 'netflix', amount: 12, category:'Entertainment'},
+  ]);
+
+  const visibleExpenses = selectedCategory? expenses.filter((e) =>e.category === selectedCategory) : expenses;
+
  
   return (
     <>
-      <ExpenseTracker />
+      <div className="mb-5">
+        <ExpenseTracker />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)}/>
+      </div>
+      <ExpenseList expenses={visibleExpenses } onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}/>
     </>
   )
 }
--- src/components/expense-tracker/components/ExpenseTracker.tsx
+++ src/components/expense-tracker/components/ExpenseTracker.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import '../App.css';
+import '../ExpenseTracker.css'
 import {z} from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod';
 import { FieldValues, useForm } from 'react-hook-form';
+import categories from '../categories';
 
 
 const schema = z.object({
-  description: z.string().min(3, {message:'This field is required'}),
-  amount: z.number({invalid_type_error:'Enter an amount'}).min(1),
-  category: z.enum(['all-categories', 'groceries', 'utilities', 'entertainment'], {message: 'A category should be selected'})
+  description: z.string().min(3, {message:'Description should be at least 3 characters'}).max(100, {message: 'max allowed chars is 100'}),
+  amount: z.number({invalid_type_error:'Enter an amount'}).min(1).max(1000, {message: 'You have exceeded highest amount'}),
+  category: z.enum(categories, {message: 'A category should be selected'})
 });
 
 type FormData = z.infer<typeof schema>
@@ -26,23 +27,22 @@ const ExpenseTracker = () => {
         <form action="submit" onSubmit={handleSubmit(onSubmit)}>
         <div className='row g-3'>
         <div className='col-12'>
-          <label htmlFor="description">Description</label>
+          <label htmlFor="description" className='form-label'>Description</label>
           <input {...register('description')} type="text" className='form-control' id="description"/>
           {errors.description && <p className='text-danger'>{errors.description.message}</p>}
         </div>
         <div className='col-12 '>
-          <label htmlFor="amount">Amount</label>
+          <label htmlFor="amount" className='form-label'>Amount</label>
           <input {...register('amount', {valueAsNumber: true})} type="number" className='form-control' id="amount"/>
           {errors.amount && <p className='text-danger'>{errors.amount.message}</p>}
         </div>
-        <div className='col-12 '>
-          <label htmlFor="category">Category</label>
+        <div className='col-12'>
+          <label htmlFor="category" className='form-label'>Category</label>
           <select {...register('category')} name="category" id="category" className='form-select'>
             <option value=""></option>
-            <option value="all-categories">All Categories</option>
-            <option value="groceries">Groceries</option>
-            <option value="utilities">Utilities</option>
-            <option value="entertainment">Entertainment</option>
+            {categories.map((category) => (
+        <option key={category} value={category}>{category}</option>
+      ))}
           </select>
           {errors.category && <p className='text-danger'>{errors.category.message}</p>}
         </div>
